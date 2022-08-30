007285a6ae6411df46fabcb19240538d0431fcf4
Chris Raphael <christopher.raphael97@gmail.com>
2022-08-30 15:31:46 +1000

    Rails Restaurant with review function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: "Le KFC", address: "KFC headquarters", phone_number: '044123324', category: 'french')
+Restaurant.create(name: 'Pizzera', address: '1 random street', phone_number: '0400000000', category: 'french')
+Restaurant.create(name: 'Sushi', address: '2 random street', phone_number: '0400000001', category: 'belgian')
+Restaurant.create(name: 'Surf Grill', address: '3 random street', phone_number: '0400000002', category: 'japanese')
+Restaurant.create(name: 'Rabbit Restaurant', address: '4 random street', phone_number: '0400000003', category: 'italian')
